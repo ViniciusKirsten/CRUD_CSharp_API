75335000f9b499323d40af3084fd1a5f589c09ac
Vinicius Mendonca <vinicius.k.mendonca@outlook.com>
2025-07-16 22:03:42 -0300

    Criando algumas rotas para o meu projeto

---
+++ Routes/PersonRoute.cs
@@ -0,0 +1,11 @@
+namespace Person.Routes;
+
+//adcionando um arquivo de controle de rotas, para que eu não seixe o meu Program lotado de informações por metodos
+public static class PersonRoute//classe responsavel por ter todos os endpoints
+{
+    public static void PersonRoutes(this WebApplication app) //com o "this" ele entende que esse metodo é um extensão
+    {
+        //nesse add.Map eu posso ter varios reursos, como (post, put, delete, patch ...)
+        app.MapGet("person", () => "Olá pessoa");
+    }
+}
