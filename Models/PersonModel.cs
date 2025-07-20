77679e4d39ee4b8b97d3acb757aab858b4c35895
Vinicius Mendonca <vinicius.k.mendonca@outlook.com>
2025-07-20 15:14:30 -0300

    Adicionando o Model Person

---
+++ Models/PersonModel.cs
@@ -0,0 +1,12 @@
+namespace Person.Models;
+
+public class PersonModel
+{
+    public PersonModel(string name)
+    {
+        Name = name;
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; init; }
+    public string Name { get; private set; }
+}
--- Routes/PersonRoute.cs
+++ Routes/PersonRoute.cs
@@ -1,3 +1,5 @@
+using Person.Models;
+
 namespace Person.Routes;
 
 //adcionando um arquivo de controle de rotas, para que eu não seixe o meu Program lotado de informações por metodos
@@ -6,6 +8,6 @@ public static class PersonRoute//classe responsavel por ter todos os endpoints
     public static void PersonRoutes(this WebApplication app) //com o "this" ele entende que esse metodo é um extensão
     {
         //nesse add.Map eu posso ter varios reursos, como (post, put, delete, patch ...)
-        app.MapGet("person", () => "Olá pessoa");
+        app.MapGet("person", () => new PersonModel("Henrique"));
     }
 }
